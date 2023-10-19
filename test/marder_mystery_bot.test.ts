d7867e0401fd884c20a75e2bc8c0de7f7be69438
sakihiro <sakihiro@toralab.org>
2023-10-19 20:35:50 +0900

    Initial commit

---
+++ test/marder_mystery_bot.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MarderMysteryBot from '../lib/marder_mystery_bot-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/marder_mystery_bot-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MarderMysteryBot.MarderMysteryBotStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
